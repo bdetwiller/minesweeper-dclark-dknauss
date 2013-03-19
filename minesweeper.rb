8a09979c8f109eafe46938d282bac12d066c4ce6
dclark+dknauss <dclark+dknauss@appacademy.io>
2013-03-18 17:57:09 -0700

    Add small and large game boards functionality

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,25 +1,33 @@
 require 'debugger'
+require 'yaml'
 class Board
-  def initialize
+  attr_reader :board_dimension
+
+  def initialize(board_size)
+    @board_dimension = (board_size == :small ? 9 : 16)
     @board = []
     @game_over = false
-    @num_bombs = 1
+    @num_bombs = (@board_dimension == 9 ? 10 : 40)
     make
   end
 
   def make
-    9.times do |i|
+    @board_dimension.times do |i|
       line = []
-      9.times do |j|
+      @board_dimension.times do |j|
         line << Tile.new
       end
       @board << line
     end
     add_bombs
-    print_bombs
   end
 
   def display
+    display_small if @board_dimension == 9
+    display_large if @board_dimension == 16
+  end
+
+  def display_small
     puts
     print '      '
     "A".upto("I") { |num| print "#{num}  " }
@@ -32,6 +40,26 @@ class Board
       puts
     end
     puts "    #{"###" * 9}##"
+    puts
+  end
+
+  def display_large
+    puts
+    print '       '
+    "A".upto("P") { |num| print "#{num}  " }
+    puts
+    puts "     #{"###" * @board_dimension}##"
+    @board.each_with_index do |row, i|
+      if i > 9
+        print "#{i}  #  "
+      else
+        print "#{i}   #  "
+      end
+      row.each { |tile| print "#{tile.display}  " }
+      print "#"
+      puts
+    end
+    puts "     #{"###" * @board_dimension}##"
     puts
   end
 
@@ -70,12 +98,7 @@ class Board
     if tile.bomb?
       lost
     else
-      bombs = 0
-      tile_neighbors(coordinates).each do |neighbor_coords|
-        x, y = neighbor_coords
-        neighbor_tile = @board[y][x]
-        bombs += 1 if neighbor_tile.bomb?
-      end
+      bombs = count_surrounding_bombs(coordinates)
       if bombs == 0
         tile.display_state = '_'
         tile_neighbors(coordinates).each { |coords| reveal(coords) }
@@ -83,6 +106,15 @@ class Board
         tile.display_state = "#{bombs}"
       end
     end
+  end
+
+  def count_surrounding_bombs(coordinates)
+    bombs = 0
+    tile_neighbors(coordinates).each do |neighbor_coords|
+      neighbor_tile = select_tile(neighbor_coords)
+      bombs += 1 if neighbor_tile.bomb?
+    end
+    bombs
   end
 
   def lost
@@ -105,7 +137,8 @@ class Board
 
   def valid_tile?(coordinates)
     x,y = coordinates
-    (0..8).include?(x) && (0..8).include?(y) &&
+    (0..(@board_dimension - 1)).include?(x) &&
+    (0..(@board_dimension - 1)).include?(y) &&
     ['*','F'].include?(@board[y][x].display)
   end
 
@@ -149,30 +182,64 @@ class Tile
 end
 
 class Game
-  def initialize
-    @board = Board.new
-  end
+  attr_accessor :board
 
   def print_welcome_shit
     puts "Welcome to Minesweeper"
-    puts "Let's play."
     puts
-    puts "Start by choosing the coordinate you want to reveal."
-    puts "Choose the coordinate prefixed by r/f (reveal/flag) (e.g. 'rA1')"
+  end
+
+  def get_load_or_new_game
+    puts "Would you like to load or start a new game?"
+    puts "Press 'l' to load, 'n' for new game."
+    answer = gets.chomp.upcase[0]
+    case answer
+    when 'L' then load_game
+    when 'N' then new_game
+    end
+  end
+
+  def load_game
+    puts "Enter the filename of the game you'd like to load."
+    puts "(e.g. my_save_game.yaml)"
+    filename = gets.chomp
+    save_game = YAML::load(File.read(filename))
+    @board = save_game.board
+    puts "Welcome Back!"
+  end
+
+  def new_game
+    puts "Would you like a small (9x9) or large (16x16) board?"
+    puts "enter 's' or 'l'"
+    size = case gets.chomp.upcase[0]
+    when 'S' then :small
+    when 'L' then :large
+    else :small
+    end
+    @board = Board.new(size)
   end
 
   def parse_coordinates(user_input)
+    # user_input = "R#{user_input}" if user_input.length == 2
     mode = case user_input[0]
     when 'R' then :reveal
     when 'F' then :flag
+    when 'Q' then :quit
+    when 'S' then :save
+    when 'H' then :help
     end
-    x = "ABCDEFGHI".index(user_input[1])
-    y = user_input[2].to_i
-    [[x, y], mode]
+    if [:reveal, :flag].include?(mode)
+      x_range = (@board.board_dimension == 9 ? "ABCDEFGHI" : "ABCDEFGHIJKLMNOP")
+      x = x_range.index(user_input[1])
+      y = user_input[2..-1].to_i
+      return [[x, y], mode]
+    else
+      [[0,0], mode]
+    end
   end
 
   def get_coordinates_and_mode
-    print "Enter coordinates (e.g. 'rA1', 'fB7'): "
+    print "Enter coordinates: "
     coordinates, mode = parse_coordinates(gets.chomp.upcase)
     until valid_input?(coordinates, mode)
       print "Please enter valid coordinates of an unchecked tile: "
@@ -182,23 +249,58 @@ class Game
   end
 
   def valid_input?(coordinates, mode)
+    return true if [:quit, :save, :help].include?(mode)
     @board.valid_tile?(coordinates) && [:reveal, :flag].include?(mode)
+  end
+
+  def take_action(coords_and_mode)
+    coords, mode = coords_and_mode
+    case mode
+    when :reveal then @board.reveal(coords)
+    when :flag then @board.flag(coords)
+    when :quit then quit
+    when :save then save
+    when :help then help
+    end
   end
 
   def play
     print_welcome_shit
+    get_load_or_new_game
     until @board.game_over? || @board.won?
       @board.display
-      coords, mode = get_coordinates_and_mode
-      @board.reveal(coords) if mode == :reveal
-      @board.flag(coords) if mode == :flag
-      @board.print_bombs
+      take_action(get_coordinates_and_mode)
     end
     if @board.won?
       puts "Congratulations, you've won!"
     else
-      puts "BOOOOOOOOOOOOOOOM"
+      puts "Game Over"
     end
+  end
+
+  def quit
+    puts "Are you sure you want to quit (y/n)?"
+    @board.lost if gets.chomp.upcase[0] == 'Y'
+  end
+
+  def clean_filename(user_input)
+    return "my_saved_game" if user_input.length < 1
+    user_input = user_input[0,140] if user_input.length > 140
+    user_input.gsub!(' ','_')
+    user_input.gsub(/\W/,'')
+  end
+
+  def save
+    puts "What do you want to call your save game?"
+    file_name = clean_filename(gets.chomp)
+    File.open("#{file_name}.yaml", 'w') do |f|
+      f.puts self.to_yaml
+    end
+  end
+
+  def help
+    puts 'Enter coordinates prefixed by "r" or "f" to reveal/flag (e.g. "rA1")'
+    puts 'or "q" to quit, "s" to save, or "h" to show this message.'
   end
 end
 
